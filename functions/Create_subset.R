810aaa6bffb49dfa4518a3ed26eb817391709cac
Anton Wasielewski <antonw@noacs.io>
2023-09-20 17:14:55 +0200

    method and code

--- functions/Create_subset.R
+++ functions/Create_subset.R
@@ -3,7 +3,7 @@ library(rofi)
 Create_subset <- function(dataset){
  
    ## creating subset
-  subset.datas <- prepared.data[, c("Gender", "DOB", "Deceased", "ed_emerg_proc", "pt_age_yrs", "ofi")]
+  subset.datas <- prepared.data[, c("Gender", "DOB", "Deceased", "ed_emerg_proc", "pt_age_yrs", "ofi", "Problemomrade_.FMP")]
   
   ## Exclude patients with missing variables
   newsubset.data <- na.omit(subset.datas)
